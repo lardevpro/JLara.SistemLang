23803e43c457d3567a09a02b7140819a3c74649f
lardevpro <lardevpro@gmail.com>
2024-12-01 00:21:40 +0100

    login-user-v1.0

--- src/JLaraSystemLeng.Domain/Progress/Progress.cs
+++ src/JLaraSystemLeng.Domain/Progress/Progress.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.Identity;
 
 namespace JLaraSystemLeng.Progress
 {
-    internal class Progress
+    public class Progres: FullAuditedAggregateRoot<Guid>
     {
+        public Guid UserId { get; set; }
+        public IdentityUser User { get; set; }
+        public DateTime? PracticeDate { get; set; }
+        public decimal? PronunciationAccuracy { get; set; }
+        public string? Recommendation { get; set; }
+        public string? DifficultyLevel {get; set; } 
     }
 }
